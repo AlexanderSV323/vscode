6a9264c17d8efe069b80aa09dfa984768a9b919a
AlexanderSV323 <azaz9t00@gmail.com>
2025-10-14 11:12:42 +0500

    Delete LR_6/1.py

--- LR_6/1.py
+++
@@ -1,6 +0,0 @@
-fio_input = input("Введите ваше ФИО через пробел: ")
-formatted_fio = " ".join(part.capitalize() for part in fio_input.split())
-if len(formatted_fio.split()) == 3:
-    print(f"Добро пожаловать {formatted_fio}")
-else:
-    print("Ошибка: пожалуйста, введите Фамилию, Имя и Отчество через пробел")
